9121fa98a1737f0e06b0c4f351e34e6890439c2c
Paterson <kpeflyn@gmail.com>
2017-06-05 19:46:21 +0000

    Adding QuestionRepository

--- src/AppBundle/Repository/QuestionRepository.php
+++ src/AppBundle/Repository/QuestionRepository.php
@@ -9,11 +9,41 @@
 namespace AppBundle\Repository;
 
 
-use AppBundle\Entity\Question;
+use AppBundle\Entity\Category;
 use Doctrine\ORM\EntityRepository;
 
 class QuestionRepository extends EntityRepository
 {
+    /**
+     * @param Category $category
+     */
+    public function removeAllChild(Category $category){
+        $this->_em->createQuery(sprintf('UPDATE %s q SET q.category = NULL WHERE q.category = :id', $this->_entityName))
+        ->execute(['id' => $category->getId()]);
+    }
+
+    /**
+     * @param array $criteria
+     */
+    public function deleteBy(array $criteria){
+        $query = $this->_em->createQuery();
+        $dql = 'DELETE %s q';
+
+        if (!empty($criteria)){
+            $where = ' WHERE';
+            $opr = ' ';
+
+            foreach ($criteria as $key=>$value){
+                $where .= sprintf('%2$sq.%1$s = :%1$s', $key, $opr);
+                $query->setParameter($key, $value);
+                $opr = ' AND ';
+            }
+
+            $dql .= $where;
+        }
+
+        $query->setDQL(sprintf($dql, $this->_entityName))->execute();
+    }
 
 
 }
